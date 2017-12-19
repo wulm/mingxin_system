877d6798c3f3cbeb9e96070a2aca4fcb8f4b51aa
“wulm1315 <397774505@qq.com>
2017-12-19 11:15:13 +0800

    ignore

--- mingxin_weixin/target/mingxin_weixin-1.0-SNAPSHOT/WEB-INF/jsp/test.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-   <title>Title</title>
-</head>
-<body>
-test22522222222
-</body>
-</html>
--- mingxin_weixin/target/mingxin_weixin-1.0-SNAPSHOT/resources/js/test.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 2017\12\17 0017.
- */
-dosave = function (){
-   alert("成功啦！");
-   // return Data();
-}
