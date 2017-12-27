e8e7051fc873fb22fae6e247041914319510b3b3
wulm <397774505@qq.com>
2017-12-27 23:19:52 +0800

    1222

--- mingxin_weixin/src/main/java/com/weixin/config/WeixinStaticData.java
+++ mingxin_weixin/src/main/java/com/weixin/config/WeixinStaticData.java
@@ -3,20 +3,37 @@ package com.weixin.config;
 import weixin.popular.bean.token.Token;
 
 public class WeixinStaticData {
+
+	/*ming134、ming1315的测试号配置地址*/
+	public static Token access_token;/*access_token*/
 	
-	/*access_token*/
-	public static Token access_token;
+
+	public final static String appID="wx34c7cde93a1c5c40";/*appID*/
 	
-	/*appID*/
-	public final static String appID="wx34c7cde93a1c5c40";
+
+	public final static String appsecret="97a28450aed0e10b0299ff88ae482bac";/*appsecret*/
 	
-	/*appsecret*/
-	public final static String appsecret="97a28450aed0e10b0299ff88ae482bac";
+
+	public final static String token="weixin_mingxin";/*Token*/
 	
-	/*Token*/
-	public final static String token="weixin_mingxin";
-	
-	/*web url*/
-	public final static String serverUrl="http://mingxin.imwork.net/mingxin_weixin/";
-	
+
+	public final static String serverUrl="http://mingxin.imwork.net/mingxin_weixin/";/*web url*/
+	/*ming134、ming1315的测试号配置地址*/
+
+
+
+
+	/*ming135、test weixin的测试号配置地址*/
+
+//	public static Token access_token;/*access_token*/
+//
+//	public final static String appID="wxa8fba1aad753cd22";/*appID*/
+//
+//	public final static String appsecret="73f63922bd780e08cff223f7a422c4f6";/*appsecret*/
+//
+//	public final static String token="weixin_mingxin";/*Token*/
+//
+//	public final static String serverUrl="http://mingxin2017.iask.in/mingxin_weixin/";/*web url*/
+
+	/*ming135、test weixin的测试号配置地址*/
 }
